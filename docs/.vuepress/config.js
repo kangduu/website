b4003a325a61b113492094d38bc4db261816cdd7
kangduu <dukang1127@163.com>
2022-04-28 00:13:39 +0800

    页面配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,18 @@
 module.exports = {
-  title: "康少的个人网站",
-  description: "",
+  title: "大眼猫的博客",
+  description: "博客,前端,个人网站,技术博客,React,Vue,JavaScript,HTML,CSS",
+  theme: "reco",
+  locales: {
+    "/": {
+      lang: "zh-CN",
+    },
+  },
+  head: [["link", { rel: "icon", href: "/logo.png" }]],
   themeConfig: {
     nav: [
-      { text: "主页", link: "/" },
+      { text: "JavaScript🔥根深蒂固", link: "/react/" },
+      { text: "React🔥深入浅出", link: "/react/" },
+      { text: "Interview🔥每日壹题", link: "/question/" },
       {
         text: "GitHub",
         link: "https://github.com/kangduu",
