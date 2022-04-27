5efd76f1f9ba10ca76f536dc419b2a6c2d2441be
kangduu <dukang1127@163.com>
2022-04-27 18:03:46 +0800

    使用VuePress搭建个人站点

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: "康少的个人网站",
+  description: "",
+  themeConfig: {
+    nav: [
+      { text: "主页", link: "/" },
+      {
+        text: "GitHub",
+        link: "https://github.com/kangduu",
+      },
+    ],
+  },
+};
